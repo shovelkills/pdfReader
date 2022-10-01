de08344906c13f4190eaa81ce77addb1e3de4d26
shovelkills <shovelkills@gmail.com>
2022-10-01 23:26:50 +1300

    Feature: Convert to list
    Converted all the text on the pdf file to a list separated by new lines

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import PyPDF2
 if __name__ == '__main__':
    pdfFile = open('practical_work_companies_list.pdf', 'rb')
    pdfReader = PyPDF2.PdfFileReader(pdfFile)
-   print(pdfReader.numPages)
-   page = pdfReader.getPage(0)
-   print(page.extractText())
+   output = ''
+   for pages in range(pdfReader.numPages):
+      page = pdfReader.getPage(pages)
+      output += page.extractText()
+   print(output.split("\n"))
